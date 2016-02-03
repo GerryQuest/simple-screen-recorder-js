a335cebefed65fe01c86d64094b253730a2bae84
Eissek <sane.gez@gmail.com>
2016-02-03 23:46:33 +0000

    added colour chaneg animation to and back

--- components/record.js
+++ components/record.js
@@ -14,16 +14,88 @@ import React, {
   DrawerLayoutAndroid,
   ToolbarAndroid,
   TouchableOpacity,
-  Alert
+  Alert,
+  Animated,
+  Easing
 } from "react-native";
 
 import Icon from "react-native-vector-icons/FontAwesome";
+var AnimateIcon = Animated.createAnimatedComponent(Icon);
 
 var Record = React.createClass ({
+  startRecord: function () {
+    
+  },
+
+  getInitialState: function () {
+   return {
+     recordButtonFade: new Animated.Value(0),
+     recordButtonShadow: new Animated.Value(0)
+   
+   };
+  },
+  runAnimation: function () {
+    // var timing = Animated.timing;
+    // Animated.sequence ([
+    //   timing (
+    // 	this.state.recordButtonFade,
+    // 	{duaration: 9000,
+    // 	 delay: 100,
+    // 	 easing: Easing.in(Easing.ease),
+    // 	 toValue: 800}),
+    //   Animated.delay(400),
+      
+    // ])
+    
+    Animated.timing(
+      this.state.recordButtonFade,
+      {duaration: 9000,
+       delay: 100,
+       easing: Easing.in(Easing.ease),
+       toValue: 1800}
+    ).start();  
+  },
+  componentDidMount: function () {
+    this.runAnimation();
+    
+    Animated.timing (
+      this.state.recordButtonShadow,
+      {duaration: 6000,
+      delay:100,
+      easing: Easing.in(Easing.ease),
+      toValue: 1}
+    ).start();
+  },
+  
   render: function () {
+
+    var color = this.state.recordButtonFade.interpolate({
+      inputRange: [0,1000, 1800],
+      outputRange: ["rgb(156,41,41)", "rgb(255,28,28)", "rgb(156,41,41)"]
+      
+    });
+    
     return (
       <View style={styles.view}>
-	<Icon name="dot-circle-o" size={100} color="#9C2929" />
+	<TouchableOpacity onPress={this.startRecord}>
+	   <AnimateIcon name="dot-circle-o" size={120} 
+            style={{alignItems: "center",
+		    alignSelf: "center",
+		    justifyContent: "center",
+		    elevation:15,
+		    opacity: this.state.recordButtonFade,
+		    color:color,
+		    borderRadius: 5,
+		    borderColor: "#000",
+		    borderWidth: 5
+		   }} />
+	   <Text style={styles.textRecord}>
+	     Press to Record
+	   </Text>
+	</TouchableOpacity>
+	<Text style={styles.recordTime}>
+	  
+        </Text>
       </View>
     );
   }
@@ -38,7 +110,12 @@ const styles = StyleSheet.create({
     justifyContent: "center"
   },
   recordButton: {
-    
+    alignItems: "center",
+    alignSelf: "center",
+    justifyContent: "center"
+  },
+  textRecord: {
+   fontSize: 20
   }
 });
 
